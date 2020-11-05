492ad0826690427bd1c16e0bc52d0df7e7cbec1c
Bryce-Rummler <rummler.b@gmail.com>
2020-11-04 20:43:37 -0700

    surprise me added

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
     <div v-if="cartEmpty()">
-        <h2>Cart is empty!</h2>
+      <h2>Cart is empty!</h2>
     </div>
+    <div class="pure-menu pure-menu-horizontal">
+        <ul class="pure-menu-list">
+          <li class="pure-menu-item">
+           <a @click="surprise()" href="#" class="pure-menu-link"
+              >Surprise me</a
+            >
+          </li>
+        </ul>
+      </div>
     <CartList :cart="cart" />
   </div>
 </template>
@@ -20,18 +29,29 @@ export default {
     }
   },
   methods: {
-      cartEmpty() {
-          if (this.$root.$data.cart.length < 1) {
-              return true;
-          }
-          else return false;
-      }
+    cartEmpty() {
+      if (this.$root.$data.cart.length < 1) {
+        return true;
+      } else return false;
+    },
+    surprise() {
+        let max = this.$root.$data.products.length;
+        let min = 1;
+        let randProductIndex = Math.floor(Math.random() * (max - min) + min);
+        console.log(randProductIndex);
+        let product = this.$root.$data.products.filter(product => product.id === randProductIndex);
+        this.$root.$data.cart.push(product[0]);
+    }
   }
 };
 </script>
 
 <style>
 h2 {
+  text-align: center;
+}
+
+.pure-menu {
     text-align: center;
 }
 </style>
