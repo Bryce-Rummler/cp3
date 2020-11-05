53162f07fbe47ccecb51856c5a7f436e1dac703f
Bryce-Rummler <rummler.b@gmail.com>
2020-11-04 20:28:22 -0700

    more features

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <div v-if="cartEmpty()">
+        <h2>Cart is empty!</h2>
+    </div>
     <CartList :cart="cart" />
   </div>
 </template>
@@ -9,12 +12,26 @@ import CartList from "../components/CartList.vue";
 export default {
   name: "Cart",
   components: {
-      CartList
+    CartList
   },
   computed: {
     cart() {
       return this.$root.$data.cart;
     }
+  },
+  methods: {
+      cartEmpty() {
+          if (this.$root.$data.cart.length < 1) {
+              return true;
+          }
+          else return false;
+      }
   }
 };
 </script>
+
+<style>
+h2 {
+    text-align: center;
+}
+</style>
